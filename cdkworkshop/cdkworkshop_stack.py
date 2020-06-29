0aafbdc91daba5bcdc8ac96dbc22d9d16a5cd546
Gasimova, Aydan <aydan.gasimova@kpn.com>
2020-06-29 14:56:02 +0200

    adds lambda to the aws_cdk stack and the function

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,8 +1,16 @@
 from aws_cdk import (
     core,
+    aws_lambda as _lambda,
 )
 
 class CdkworkshopStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
